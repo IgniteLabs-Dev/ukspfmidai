8cbd05a407fccb0caac3bb010889685c2b8a8252
WahyuNT <wahyuntx28@gmail.com>
2026-06-03 22:37:02 +0700

    feat: add new SVG icons and update footer styling for improved UI

--- resources/views/components/footer-home.blade.php
+++ resources/views/components/footer-home.blade.php
@@ -1,5 +1,5 @@
-<footer class="bg-gray-900 text-white    py-4 text-center">
-    <span class="text-gray-300">{{ now()->year }} - Support by Pratama Vision</span>
+<footer class="bg-gray-300 text-black    py-4 text-center">
+    <span class="text-gray-700">{{ now()->year }} - Support by Pratama Vision</span>
 </footer>
 
 </body>
